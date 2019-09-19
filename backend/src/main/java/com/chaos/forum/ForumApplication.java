b5513594fe91406cda138f9107e0f7e3ff241da1
Sersph <1033278494@qq.com>
2019-09-19 17:07:08 +0800

    后端项目创建

---
+++ backend/src/main/java/com/chaos/forum/ForumApplication.java
@@ -0,0 +1,14 @@
+package com.chaos.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello springboot2");
+        SpringApplication.run(ForumApplication.class, args);
+    }
+
+}
