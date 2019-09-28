3aa61fd6db7952578b2e6fc4ff166649be2eca80
Sersph <1033278494@qq.com>
2019-09-28 19:47:48 +0800

    修复文章接口

--- backend/src/main/java/com/chaos/forum/config/GlobalCorsConfig.java
+++ backend/src/main/java/com/chaos/forum/config/GlobalCorsConfig.java
@@ -2,6 +2,7 @@ package com.chaos.forum.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,13 +25,13 @@ public class GlobalCorsConfig {
                 //添加映射路径
                 registry.addMapping("/**")
                         //放行哪些原始域
-                        .allowedOrigins("*")
-                        //是否发送Cookie信息
-                        .allowCredentials(true)
+                        .allowedOrigins("http://127.0.0.1:3000")
                         //放行哪些原始域(请求方式)
-                        .allowedMethods("GET","POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         //放行哪些原始域(头部信息)
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        //是否发送Cookie信息
+                        .allowCredentials(true);
             }
         };
     }
