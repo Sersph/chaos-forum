78c2d53a5a013e4c14949fefab8aabaf79fde790
midoushitongtong <1092879991@qq.com>
2019-10-15 12:44:40 +0800

    更改模拟接口为正式接口

--- frontend/front/src/api/post/index.ts
+++ frontend/front/src/api/post/index.ts
@@ -6,65 +6,65 @@ const api: PostAPI = {
   insertPost: (data: any): object => {
     return ajax(
       'POST',
-      `${config.MOCK_API_ROOT}/frontend/article/sava`,
+      `${config.API_ROOT}/frontend/article/sava`,
       data
     );
   },
   agreePost: (data: any): object => {
     return ajax(
       'POST',
-      `${config.MOCK_API_ROOT}/frontend/article/like`,
+      `${config.API_ROOT}/frontend/article/like`,
       data
     );
   },
   selectAgreePostList: (): object => {
     return ajax(
       'GET',
-      `${config.MOCK_API_ROOT}/frontend/article/selectLike`
+      `${config.API_ROOT}/frontend/article/selectLike`
     );
   },
   selectPost: (id: any): object => {
     return ajax(
       'GET',
-      `${config.MOCK_API_ROOT}/frontend/article/getOne/${id}`
+      `${config.API_ROOT}/frontend/article/getOne/${id}`
     );
   },
   deletePost: (id: any): object => {
     return ajax(
       'DELETE',
-      `${config.MOCK_API_ROOT}/frontend/article/delete/${id}`
+      `${config.API_ROOT}/frontend/article/delete/${id}`
     );
   },
   selectPostList: (data: any): object => {
     return ajax(
       'GET',
-      `${config.MOCK_API_ROOT}/frontend/article/getAll`,
+      `${config.API_ROOT}/frontend/article/getAll`,
       data
     );
   },
   selectAllPostCategoryList: (): object => {
     return ajax(
       'GET',
-      `${config.MOCK_API_ROOT}/frontend/category/all`
+      `${config.API_ROOT}/frontend/category/all`
     );
   },
   insertPostMessage: (data: any): object => {
     return ajax(
       'POST',
-      `${config.MOCK_API_ROOT}/frontend/article/saveComment`,
+      `${config.API_ROOT}/frontend/article/saveComment`,
       data
     );
   },
   deletePostMessage: (id: any): object => {
     return ajax(
       'DELETE',
-      `${config.MOCK_API_ROOT}/frontend/article/delentComment/${id}`
+      `${config.API_ROOT}/frontend/article/delentComment/${id}`
     );
   },
   selectPostMessage: (id: any, data: any): object => {
     return ajax(
       'GET',
-      `${config.MOCK_API_ROOT}/frontend/article/getComment/${id}`,
+      `${config.API_ROOT}/frontend/article/getComment/${id}`,
       data
     );
   },
--- frontend/front/src/config/dev.env.ts
+++ frontend/front/src/config/dev.env.ts
@@ -6,6 +6,5 @@ export default {
   // OSS API 请求的 url
   API_OSS_ROOT: 'https://project.yyccyy.com/auction-frontend/api/oss',
   // 模拟 API 请求 url
-  // MOCK_API_ROOT: 'http://mock.yyccyy.com/mock/5c89ee1c51962650daa7ce45/chaos-forum-mock-api'
-  MOCK_API_ROOT: 'http://10.0.18.37:8080'
+  MOCK_API_ROOT: 'http://mock.yyccyy.com/mock/5c89ee1c51962650daa7ce45/chaos-forum-mock-api'
 };
